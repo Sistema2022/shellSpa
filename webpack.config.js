cd817a9ade12a50961e9f0db19cc6d246c5e270c
RONALDO ROJAS AYALA <ronaldorojasayala@192.168.0.102>
2022-12-02 23:18:28 -0400

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = (webpackConfigEnv, argv) => {
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
     output: {
-      publicPath: "src/assets"
+      publicPath: "auto"
     },
     plugins: [
       new HtmlWebpackPlugin({
